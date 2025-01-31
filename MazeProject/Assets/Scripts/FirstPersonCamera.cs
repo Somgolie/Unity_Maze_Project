c6d15aad586ed36b1d70e51b38c6a0e0cfb70828
Somgolie <sejinkeonye@algomau.ca>
2025-01-31 18:13:42 -0500

    Fixed the playermovement
    The player was not rotating with the camera.

--- MazeProject/Assets/Scripts/FirstPersonCamera.cs
+++ MazeProject/Assets/Scripts/FirstPersonCamera.cs
@@ -4,23 +4,44 @@ using UnityEngine;
 
 public class FirstPersonCamera : MonoBehaviour
 {
-    public Transform Player;
-    public float Mouse_sensitivity = 2f;
-    float Cam_Vertical_Rotation = 0f;
+    public float senX;
+    public float senY;
+
+    public Transform Orientation;
+
+    float xRotation;
+    float yRotation;
 
     private void Update()
     {
-        //Get mouse input
-        float input_X = Input.GetAxis("Mouse X")* Mouse_sensitivity;
-        float input_Y = Input.GetAxis("Mouse Y")* Mouse_sensitivity;
+        float mouseX = Input.GetAxisRaw("Mouse X") * Time.deltaTime * senX;
+        float mouseY = Input.GetAxisRaw("Mouse Y") * Time.deltaTime * senY;
 
-        //rotate camera
-        Cam_Vertical_Rotation -= input_Y;
- 
-        //clamp camera
-        Cam_Vertical_Rotation = Mathf.Clamp(Cam_Vertical_Rotation, -90f, 90f);
-        transform.localEulerAngles = Vector3.right * Cam_Vertical_Rotation;
-
-        Player.Rotate(Vector3.up * input_X);
+        yRotation += mouseX;
+        xRotation -= mouseY;
+        xRotation = Mathf.Clamp(xRotation, -90f, 90f);
+        //rotate cam and player
+        transform.rotation = Quaternion.Euler(xRotation, yRotation, 0);
+        Orientation.rotation = Quaternion.Euler(0, yRotation, 0);
     }
+
+    /*    public Transform Player;
+        public float Mouse_sensitivity = 2f;
+        float Cam_Vertical_Rotation = 0f;
+
+        private void Update()
+        {
+            //Get mouse input
+            float input_X = Input.GetAxis("Mouse X")* Mouse_sensitivity;
+            float input_Y = Input.GetAxis("Mouse Y")* Mouse_sensitivity;
+
+            //rotate camera
+            Cam_Vertical_Rotation -= input_X;
+
+            //clamp camera
+            Cam_Vertical_Rotation = Mathf.Clamp(Cam_Vertical_Rotation, -90f, 90f);
+            transform.localEulerAngles = Vector3.right * Cam_Vertical_Rotation;
+
+            Player.Rotate(Vector3.up * input_X);
+        }*/
 }
--- MazeProject/Assets/Scripts/Player_Movement.cs
+++ MazeProject/Assets/Scripts/Player_Movement.cs
@@ -5,20 +5,54 @@ using UnityEngine;
 public class Player_Movement : MonoBehaviour
 {
     public float moveSpeed = 10f;
-    private Rigidbody rb;
+    public float movedrag;
+    public Transform orientation;
+
+    float horizontalInput;
+    float verticalInput;
+
+    Vector3 movedirection;
+
+    Rigidbody rb;
 
     private void Start()
     {
         rb = GetComponent<Rigidbody>();
+        rb.freezeRotation = true;
     }
 
+    private void Update()
+    {
+        MyInput();
+        rb.linearDamping = movedrag;
+    }
     private void FixedUpdate()
     {
-        float horizontalInput = Input.GetAxis("Horizontal");
-        float verticalInput = Input.GetAxis("Vertical");
-
-        Vector3 movement = new Vector3(horizontalInput, 0f, verticalInput) * moveSpeed;
-
-        rb.MovePosition(rb.position + movement * Time.fixedDeltaTime);
+        moveplayer();
     }
+    private void MyInput()
+    {
+        horizontalInput = Input.GetAxisRaw("Horizontal");
+        verticalInput = Input.GetAxisRaw("Vertical");
+    }
+
+    private void moveplayer()
+    {
+        movedirection = orientation.forward * verticalInput + orientation.right * horizontalInput;
+        rb.AddForce(movedirection.normalized * moveSpeed * 10f, ForceMode.Force);
+    }
+    /*    private void Start()
+        {
+            rb = GetComponent<Rigidbody>();
+        }
+
+        private void FixedUpdate()
+        {
+            float horizontalInput = Input.GetAxis("Horizontal");
+            float verticalInput = Input.GetAxis("Vertical");
+
+            Vector3 movement = new Vector3(horizontalInput, 0f, verticalInput) * moveSpeed;
+
+            rb.MovePosition(rb.position + movement * Time.fixedDeltaTime);
+        }*/
 }
