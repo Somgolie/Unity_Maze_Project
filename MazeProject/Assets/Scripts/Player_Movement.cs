a7fa9bc01322df7f5fda65bcc6a460b0596187b5
Somgolie <sejinkeonye@algomau.ca>
2025-02-17 02:15:40 -0500

    Added the maze algorithm
    Added the maze, where the player starts at 0,0. Press X to switch to the Debug camera. The current maze is randomly generated.

--- MazeProject/Assets/Scripts/Player_Movement.cs
+++ MazeProject/Assets/Scripts/Player_Movement.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 
 public class Player_Movement : MonoBehaviour
 {
+    public Camera MainCamera;
+    public Camera DebugCamera;
     public float moveSpeed = 10f;
     public float movedrag;
     public Transform orientation;
@@ -11,12 +13,27 @@ public class Player_Movement : MonoBehaviour
     float horizontalInput;
     float verticalInput;
 
+   
+
     Vector3 movedirection;
 
     Rigidbody rb;
+    public void ShowOverheadView()
+    {
+        MainCamera.enabled = false;
+        DebugCamera.enabled = true;
+    }
 
+    // Call this function to enable FPS camera,
+    // and disable overhead camera.
+    public void ShowFirstPersonView()
+    {
+        MainCamera.enabled = true;
+        DebugCamera.enabled = false;
+    }
     private void Start()
     {
+        ShowFirstPersonView();
         rb = GetComponent<Rigidbody>();
         rb.freezeRotation = true;
     }
@@ -25,6 +42,17 @@ public class Player_Movement : MonoBehaviour
     {
         MyInput();
         rb.linearDamping = movedrag;
+        if (Input.GetKeyDown(KeyCode.X))
+        {
+            if (MainCamera.enabled == true)
+            {
+                ShowOverheadView();
+            }
+            if (MainCamera.enabled == false)
+            {
+                ShowFirstPersonView();
+            }
+        }
     }
     private void FixedUpdate()
     {
